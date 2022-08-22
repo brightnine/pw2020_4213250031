52ff90783ae8c51ddf4962251bc3c8d1bfb1ad7e
Bright Nine <brightnine9@gmail.com>
2022-08-22 22:20:19 +0700

    menambah fitur create

--- kuliah/pertemuan10/functions.php
+++ kuliah/pertemuan10/functions.php
@@ -4,6 +4,9 @@ function koneksi(){
   return mysqli_connect("localhost", "root", "", "pw_4213250031");
 }
 
+
+
+
 function query($query){
   $conn = koneksi();
 
@@ -17,4 +20,30 @@ function query($query){
   return $rows;
 }
 
+
+
+
+function tambah($data){
+
+  $conn = koneksi();
+
+  $nama = htmlspecialchars($data['nama']);
+  $nrp = htmlspecialchars($data['nrp']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO 
+            mahasiswa
+            VALUES (null, '$nama', '$nrp', '$email', '$jurusan', '$gambar')";
+  
+  mysqli_query($conn, $query);
+  echo mysqli_error($conn);
+
+  echo "<script>
+          alert('Data berhasil ditambahkan');
+          document.location.href = 'latihan3.php';
+        </script>";
+  
+}
 ?>
---
+++ kuliah/pertemuan10/tambah.php
@@ -0,0 +1,65 @@
+<?php 
+require 'functions.php';
+
+if(isset($_POST['tambah']))
+{
+  tambah($_POST);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tambah data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Tambah data Mahasiswa</h3>
+  <form action="" method="post">
+    <ul>
+      <li>
+        <label>
+          Nama:
+          <input type="text" name="nama" required autofocus>
+        </label>
+      </li>
+
+      <li>
+        <label>
+          NRP:
+          <input type="text" name="nrp" required>
+        </label>
+      </li>
+
+      <li>
+        <label>
+          Email:
+          <input type="text" name="email" required>
+        </label>
+      </li>
+
+      <li>
+        <label>
+          Jurusan:
+          <input type="text" name="jurusan" required>
+        </label>
+      </li>
+
+      <li>
+        <label>
+          Gambar:
+          <input type="text" name="gambar" required>
+        </label>
+      </li>
+    </ul>
+
+    <button type="submit" name="tambah">Tambah Data</button>
+  </form>
+  <a href="latihan3.php">Kembali ke halaman sebelumnya</a>
+</body>
+
+</html>
